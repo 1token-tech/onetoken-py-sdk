57365e689ab3689402eb634acee789d9b32c3826
Youze Tang <tangyouze@gmail.com>
2018-04-04 15:33:28 +0800

    update version

--- tasks.py
+++ tasks.py
@@ -5,6 +5,9 @@ import invoke
 def upload(ctx):
     ctx.run('python setup.py sdist bdist_wheel upload')
     ctx.run('rm -rf build dist onetoken_trade.egg-info')
+    ctx.run('git add . ')
+    ctx.run('git commit -a -m "update version" ')
+    ctx.run('git push')
 
 
 @invoke.task
