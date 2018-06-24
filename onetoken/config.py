a97e0935beeb3b4b60266b08b07111d66e220f5a
Youze Tang <tangyouze@gmail.com>
2018-06-24 14:46:52 +0800

    change network to cloudflare cdn

--- onetoken/config.py
+++ onetoken/config.py
@@ -1,13 +1,13 @@
 class Config:
-    HOST_REST = 'https://api.1token.trade/v1'
-    TRADE_HOST = 'https://api.1token.trade/v1/trade'
-    TRADE_HOST_WS = 'wss://api.1token.trade/v1/ws/trade'
-    TICK_HOST_WS = 'wss://api.1token.trade/v1/ws/tick'
+    HOST_REST = 'https://1token.trade/api/v1'
+    TRADE_HOST = 'https://1token.trade/api/v1/trade'
+    TRADE_HOST_WS = 'wss://1token.trade/api/v1/ws/trade'
+    TICK_HOST_WS = 'wss://1token.trade/api/v1/ws/tick'
 
     @classmethod
-    def change_host(cls, target='1token.trade/api/', nossl=False):
+    def change_host(cls, target='1token.trade/', nossl=False):
         for item in ['TRADE_HOST', 'TRADE_HOST_WS', 'TICK_HOST_WS', 'HOST_REST']:
-            new = getattr(cls, item).replace('api.1token.trade/', target)
+            new = getattr(cls, item).replace('1token.trade/', target)
             if nossl:
                 new = new.replace('https://', 'http://')
                 new = new.replace('wss://', 'ws://')
