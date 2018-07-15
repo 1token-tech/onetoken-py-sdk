79273985f2938491d1f763f4eed71d4c61008463
Youze Tang <tangyouze@gmail.com>
2018-07-15 16:35:59 +0800

    update post example

--- tests/test_sign.py
+++ tests/test_sign.py
@@ -8,6 +8,6 @@ def test_sign_no_body():
 
 
 def test_sign_with_body():
-    r = account.gen_sign(secret='this-is-long-secret', verb='GET', url='/okex/demo/info', nonce='this-is-nonce',
-                         data_str='this-is-body')
-    assert r == '67251a0dcd967b45f3282f22607d77f843453bd7455913c57cc8dd0110453217'
+    r = account.gen_sign(secret='this-is-long-secret', verb='POST', url='/okex/demo/info', nonce='this-is-nonce',
+                         data_str='{"price": 0.1,     "amount": 0.2}')
+    assert r == 'd75535f8f5e2d21dd5e5a0e8609ef56e3177d55f661dfc51b458b9d7ada711dc'
