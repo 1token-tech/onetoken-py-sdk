356aac67ba181c07b75e9eebac4f5107173ddc0f
Youze Tang <tangyouze@gmail.com>
2018-09-02 15:45:00 +0800

    update deps

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import re
 from setuptools import setup, find_packages
+from codecs import open
+
 
 with open('onetoken/__init__.py', 'r', encoding='utf8') as fd:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
@@ -14,12 +16,38 @@ setup(name='onetoken',
       packages=find_packages(),
       version=version,
       description='OneToken Trade System Python SDK',
-      install_requires=[
-          'arrow',
-          'docopt',
-          'PyJWT',
-          'PyYAML',
-          'aiohttp==3.1.3',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Financial and Insurance Industry',
+          'Intended Audience :: Information Technology',
+          'Topic :: Software Development :: Build Tools',
+          'Topic :: Office/Business :: Financial :: Investment',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Operating System :: OS Independent',
+          'Environment :: Console'
       ],
+      install_requires=[
+          'arrow==0.12.1',
+          'docopt==0.6.2',
+          'PyJWT==1.6.4',
+          'PyYAML==3.13',
+      ],
+      extras_require={
+          ':python_version>="3.6.0"': [
+              'aiohttp==3.1.3'
+          ],
+          'qa': [
+              'flake8==3.5.0'
+          ],
+          'doc': [
+              'Sphinx==1.7.0'
+          ]
+      },
       zip_safe=False,
       )
