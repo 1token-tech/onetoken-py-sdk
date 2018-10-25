073dad9d1510f76923c627c2a8ea1bb8e2696b27
Yifei <yifei.shen@yahoo.com>
2018-10-25 17:40:41 +0800

    add model Error, DealtTrans

--- onetoken/model.py
+++ onetoken/model.py
@@ -1,5 +1,5 @@
 import json
-
+import logging
 import arrow
 import dateutil
 import dateutil.parser
@@ -389,3 +389,111 @@ class Order:
     ALL_STATUSES = []
     ALL_STATUSES.extend(ACTIVE_STATUS)
     ALL_STATUSES.extend(END_STATUSES)
+
+
+class DealtTrans:
+    BUY = 'b'
+    SELL = 's'
+    MAKER = 'maker'
+    TAKER = 'taker'
+
+    def __init__(self, exchange_tid=None, exchange_oid=None, client_oid=None, dealt_price=None, bs=None,
+                 dealt_amount=None, commission=None, commission_currency=None, dealt_type=None, exchange_update=None,
+                 tags=None, account=None, contract=None):
+        self.client_oid = client_oid
+        self.dealt_price = dealt_price
+        self.bs = bs
+        self.dealt_amount = dealt_amount
+        self.exchange_oid = exchange_oid
+        self.exchange_tid = exchange_tid
+        self.commission = commission
+        self.commission_currency = commission_currency
+        self.dealt_type = dealt_type
+        self.exchange_update = exchange_update
+        self.tags = tags
+        self.account = account
+        self.contract = contract
+
+    def to_dict(self):
+        return {
+            'client_oid': self.client_oid,
+            'dealt_pr:ce': self.dealt_price,
+            'bs': self.bs,
+            'dealt_amount': self.dealt_amount,
+            'exchange_oid': self.exchange_oid,
+            'exchange_tid': self.exchange_tid,
+            'commission': self.commission,
+            'commission_currency': self.commission_currency,
+            'dealt_type': self.dealt_type,
+            'exchange_update': self.exchange_update,
+            'tags': self.tags,
+            'account': self.account,
+            'contract': self.contract
+        }
+
+    @classmethod
+    def from_dict(cls, dct):
+        client_oid = dct['client_oid']
+        dealt_price = dct['dealt_price']
+        bs = dct['bs']
+        dealt_amount = dct['dealt_amount']
+        exchange_oid = dct['exchange_oid']
+        exchange_tid = dct['exchange_tid']
+        commission = dct['commission']
+        commission_currency = dct['commission_currency']
+        dealt_type = dct['dealt_type']
+        exchange_update = dct['exchange_update']
+        tags = dct['tags']
+        account = dct['account']
+        contract = dct['contract']
+        return DealtTrans(exchange_tid=exchange_tid, exchange_oid=exchange_oid, client_oid=client_oid,
+                          dealt_price=dealt_price, bs=bs,
+                          dealt_amount=dealt_amount, commission=commission, commission_currency=commission_currency,
+                          dealt_type=dealt_type, exchange_update=exchange_update,
+                          tags=tags, account=account, contract=contract)
+
+
+class Error:
+
+    def __init__(self, code, message='', status=400, data=None):
+        self.code = code
+        self.message = message
+        self.status = status
+        self.data = data
+
+    def to_dict(self):
+        return {
+            'code': self.code,
+            'message': self.message,
+            'status': self.status,
+            'data': self.data
+        }
+
+    @classmethod
+    def from_dict(cls, dct):
+        code = dct['code']
+        message = dct['message']
+        status = dct['status']
+        data = dct.get('data')
+        return Error(code, message, status, data)
+
+    @classmethod
+    def from_http_error(cls, http_error):
+        """
+
+        :param http_error:
+        :return:
+        """
+        code = http_error.code
+        message = http_error.message
+        data = None
+        try:
+            if message:
+                message = json.loads(message)
+                code = message['code']
+                message = message['message']
+                data = message.get('date')
+        except Exception as e:
+            logging.exception('parse err message exception', e)
+            raise e
+        return Error(code=code, message=message, data=data)
