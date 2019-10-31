778d02f1e6995a9d52bb0399b30e278bbfc1fc67
liuzk <liuzk@1token.trade>
2019-10-31 14:03:56 +0800

    update version

--- onetoken/__init__.py
+++ onetoken/__init__.py
@@ -1,10 +1,10 @@
 from . import autil
 from . import quote
 from . import util
+from .account import Account, Info
 from .config import Config
 from .logger import log, log_level
 from .model import *
 from .rpcutil import ServiceError, HTTPError, Code, Const
-from .account import Account, Info
 
-__version__ = '0.2.191031.1'
+__version__ = '0.2.191031.2'
