79637939d4726513983a27f7a8488a7b64eace3f
tyz <tyz@1token.trade>
2019-10-31 11:40:29 +0800

    logger update

--- onetoken/logger.py
+++ onetoken/logger.py
@@ -1,9 +1,10 @@
-import functools
-import sys
 import logging
+import sys
+
+import functools
 from pathlib import Path
 
-log = logging.getLogger('ots')
+log = logging.getLogger('ot')
 
 
 def set_log():
@@ -20,15 +21,20 @@ def set_log():
     def wrap(level, orig):
         @functools.wraps(orig)
         def new_func(*args, **kwargs):
-            if level < log.level:
-                return
-            left = ' '.join(str(x) for x in args)
-            right = ' '.join('{}={}'.format(k, v) for k, v in kwargs.items())
-            new = ' '.join(filter(None, [left, right]))
-            import inspect
-            r = inspect.stack()[1]
-            new = f'[{Path(r.filename).name}:{r.lineno}] {new}'
-            orig(new)
+            try:
+                if level < log.level:
+                    return
+                left = ' '.join(str(x) for x in args)
+                right = ' '.join('{}={}'.format(k, v) for k, v in kwargs.items())
+                new = ' '.join(filter(None, [left, right]))
+                import inspect
+                r = inspect.stack()[1]
+                new = f'[{Path(r.filename).name}:{r.lineno}] {new}'
+                orig(new)
+            except Exception as e:
+                print('onetoken log fail', e, type(e))
+                import traceback
+                traceback.print_exc()
 
         return new_func
 
